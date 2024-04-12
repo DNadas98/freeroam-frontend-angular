956abdbb9ff057ed5cda72f3b0c4476d70adca21
DNadas98 <daniel.nadas.123@gmail.com>
2024-04-12 12:34:40 +0200

    configure tailwind, add header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatMenu, MatMenuItem, MatMenuTrigger} from "@angular/material/menu";
+import {MatIcon} from "@angular/material/icon";
+import {MatBadge} from "@angular/material/badge";
+import {MatButton, MatIconButton} from "@angular/material/button";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbar,
+    MatMenuTrigger,
+    MatIcon,
+    MatBadge,
+    MatMenu,
+    MatIconButton,
+    MatButton,
+    MatMenuItem,
+    RouterLink
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
